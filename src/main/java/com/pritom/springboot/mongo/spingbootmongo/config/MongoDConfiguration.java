7d69f1fe242490cb41441eb5f15bb0caa7b45738
Pritom Deb <pritom72@gmail.com>
2018-06-16 16:32:23 +0530

    Added Command line runner to run on load

--- src/main/java/com/pritom/springboot/mongo/spingbootmongo/config/MongoDConfiguration.java
+++ src/main/java/com/pritom/springboot/mongo/spingbootmongo/config/MongoDConfiguration.java
@@ -1,9 +1,19 @@
 package com.pritom.springboot.mongo.spingbootmongo.config;
 
+import com.pritom.springboot.mongo.spingbootmongo.document.User;
+import com.pritom.springboot.mongo.spingbootmongo.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @Configuration
 @EnableMongoRepositories(basePackageClasses = UserRepository.class)
 public class MongoDConfiguration {
+
+    CommandLineRunner commandLineRunner (UserRepository userRepository){
+        return args -> {
+            userRepository.save(new User(1, "Peter", "Google", 1200L));
+            userRepository.save(new User(1, "Sam", "Facebook", 1800L));
+        };
+    }
 }
