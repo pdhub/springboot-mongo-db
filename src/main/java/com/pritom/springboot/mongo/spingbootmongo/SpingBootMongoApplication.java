45f9fd6f9c3cd0b6a6eacf089be437160334a490
Pritom Deb <pritom72@gmail.com>
2018-06-16 16:11:39 +0530

    Added the basic framework by spring initializer

---
+++ src/main/java/com/pritom/springboot/mongo/spingbootmongo/SpingBootMongoApplication.java
@@ -0,0 +1,12 @@
+package com.pritom.springboot.mongo.spingbootmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingBootMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpingBootMongoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/pritom/springboot/mongo/spingbootmongo/SpingBootMongoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pritom.springboot.mongo.spingbootmongo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpingBootMongoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
